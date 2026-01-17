ea72727e3997ae8d8f4ed84bb7c019487074a9e3
Vibec3der <matchhasbeenfound@gmail.com>
2026-01-17 04:42:27 +0000

    stupid src.

--- spanish/active/uv/uv.config.js
+++ spanish/active/uv/uv.config.js
@@ -1,27 +1,12 @@
 // UV config
-
-// L bare stealers, we use wisp ;)
-//  ⠀⠀⠀       ⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-//  ⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠟⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀
-//  ⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣳⠖⠳⣄⡀⠀⠀⠀⠀⠀⠀
-//  ⠀⠀⠀⠀⡤⠒⠀⠂⡾⠉⠀⠀⠀⠀⠉⢳⡄⣄⡀⠀⠀
-//  ⠀⠀⠀⢸⡥⠀⣀⡼⢁⠀⢰⡄⠀⡄⠀⠀⣧⠀⠙⡆⠀
-//  ⠀⢀⣠⠼⠗⠚⠉⠠⠋⠀⠀⢷⣠⣧⠀⠀⠈⠳⢤⣇⠀
-//  ⠀⣾⣟⠒⠦⣄⠀⠀⠀⣠⡴⠋⠁⢈⠛⢦⣄⣠⣴⣾⣷
-//  ⢀⡟⠙⢶⣤⠬⠷⣼⡏⠉⠉⢩⡍⠹⠦⢤⣿⣤⣨⣿⠈
-//  ⡾⣡⠆⠁⠀⠀⠀⢠⡀⠀⠀⠀⢱⡀⠀⠂⠙⠎⠻⡅⠀
-//  ⢿⠁⠒⣤⠤⣤⣀⠀⢧⠀⠀⠀⣸⠃⠀⠀⠀⡶⣤⣽⠀
-//  ⠈⠳⣴⡇⠀⠀⠈⠛⢦⣄⣠⠾⢿⣄⣀⣠⠾⣡⠞⠁⠀
-//  ⠀⠀⠈⠉⠉⠉⠁⢧⢠⠟⣽⠀⢿⠀⢧⢰⡈⠀⠀⠀⠀
-//  ⠀⠀⠀⠀⠀⠀⠐⢫⠏⢸⠁⠀⠈⢳⠘⢧⣙⢦⠀⠀⠀
-
 self.__uv$config = {
-  prefix: "./spanish/active/uv/service/",
+  // prefix used by the UV service (relative to index.html location)
+  prefix: "uv/service/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "./spanish/active/uv/uv.handler.js",
-  client: "./spanish/active/uv/uv.client.js",
-  bundle: "./spanish/active/uv/uv.bundle.js",
-  config: "./spanish/active/uv/uv.config.js",
-  sw: "./spanish/active/uv/uv.sw.js",
+  handler: "uv/uv.handler.js",
+  client: "uv/uv.client.js",
+  bundle: "uv/uv.bundle.js",
+  config: "uv/uv.config.js",
+  sw: "uv/uv.sw.js",
 };
